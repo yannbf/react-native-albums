c0ada1446e6c70757fd1a947d3a6aaf2c114118c
Yann Braga <yannbf@gmail.com>
2017-07-08 13:55:46 -0300

    Add Header component WIP

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// Import libraries for making the component
+import React from 'react';
+import {
+    Text
+} from 'react-native';
+
+// Make the component
+const Header = () => {
+    return <Text>Albums!</Text>;
+};
+
+// Make the component available to other parts of the app
+export default Header;
