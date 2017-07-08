1d39bcef3d25b79e1db789c2b6352b3f0023117c
Yann Braga <yannbf@gmail.com>
2017-07-08 14:59:18 -0300

    Add props to header

--- index.android.js
+++ index.android.js
@@ -3,13 +3,13 @@
 // Import a library to help create a component
 import React from 'react';
 import {
-  Text,
   AppRegistry
 } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-  <Text>Some Text</Text>
+  <Header headerText={'Albums'} />
 );
 
 // Render the component to the device
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,38 @@
 // Import libraries for making the component
 import React from 'react';
 import {
-    Text
+    Text,
+    View
 } from 'react-native';
 
 // Make the component
-const Header = () => {
-    return <Text>Albums!</Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor : '#F0F0F0',
+        justifyContent  : 'center',
+        alignItems      : 'center',
+        height          : 60,
+        paddingTop      : 15,
+        shadowColor     : '#000',
+        shadowOffset    : {
+            width  : 0,
+            height : 5,
+        },
+        shadowOpacity: 1
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 // Make the component available to other parts of the app
